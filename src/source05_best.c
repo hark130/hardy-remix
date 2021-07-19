957fdbf429f176b2e3a8abc85ceb60dac503bcdd
Joseph Harkleroad <hark130@yahoo.com>
2021-07-19 14:48:19 -0500

    development: Updated source05_best.c
    Selectively copy/pasted from source04_best.c
    
    TD: DDN... fill in the includes as necessary later

--- src/source05_best.c
+++ src/source05_best.c
@@ -1,1 +1,190 @@
+/*
+ *
+ * 1. Take a filename as a command line argument
+ * 2. Read file contents
+ * 3. Print the file contents
+ */
 
+#define BINARY_NAME "source05_best.bin"
+
+
+char *get_filename(int argc, char *argv[]);
+char *parse_args(int argc, char *argv[]);
+void print_usage(void);
+char *validate_arg(char *argOne);
+int verify_filename(char *filename);
+
+
+int main(int argc, char *argv[])
+{
+    // LOCAL VARIABLES
+    char *filename = NULL;    // Flow control
+    int file_exists = 0;      // Return value from verify_filename(): 1 exists, 0 missing, -1 error
+    int success = 0;          // 0 Success, -1 Bad input, -2 Error
+
+    // DO IT
+    // 1. PARSE ARGUMENTS
+    filename = get_filename(argc, argv);
+
+    // 2. VERIFY ENVIRONMENT
+    if (filename)
+    {
+        file_exists = verify_filename(filename);
+        if (0 == file_exists)
+        {
+            fprintf(stderr, "Unable to find %s\n", filename);
+            print_usage();
+            filename = NULL;  // Used to control flow
+            success = -1;
+        }
+        else if (-1 == file_exists)
+        {
+            fprintf(stderr, "Error verifying %s\n", filename);
+            filename = NULL;  // Used to control flow
+            success = -2;
+        }
+    }
+
+    // 3. READ FILE
+    if (filename)
+    {
+        if(!authenticate(PASSWORD))
+        {
+            // Read file
+            // Print the file contents
+            fprintf(stderr, "%s\n", filename);  // DEBUGGING            
+        }
+        else
+        {
+            success = -1;  // Bad password
+        }
+    }
+
+    // DONE
+    return success;
+}
+
+
+/*
+ *  Return the filename argument
+ */
+char *get_filename(int argc, char *argv[])
+{
+    // LOCAL VARIABLES
+    char *argOne = NULL;    // Argument 1 from argv
+    char *filename = NULL;  // Filename, if it exists
+
+    // PARSE ARGS
+    argOne = parse_args(argc, argv);
+
+    // CHECK ARGS
+    filename = validate_arg(argOne);
+
+    // DONE
+    return filename;
+}
+
+
+/*
+ *  Validate arguments.  Return argument 1 (as appropriate).
+ */
+char *parse_args(int argc, char *argv[])
+{
+    // LOCAL VARIABLES
+    char *argOne = NULL;
+
+    // VALIDATE ARGUMENTS
+
+    // Verify argc
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invalid number of arguments!\n");
+    }
+    else if (argv[0] && argv[1])
+    {
+        // Verify arg 0
+        if (strstr(argv[0], BINARY_NAME) != (strlen(argv[0]) - strlen(BINARY_NAME) + argv[0]))
+        {
+            fprintf(stderr, "Invalid binary name!\n");
+        }
+        else
+        {
+            argOne = argv[1];
+        }
+    }
+
+    // ERROR
+    if (!argOne)
+    {
+        print_usage();
+    }
+
+    // DONE
+    return argOne;
+}
+
+
+void print_usage(void)
+{
+    fprintf(stderr, "usage: app_name [options] input_file\n");
+    fprintf(stderr, "\toptions:\n");
+    fprintf(stderr, "\t\t-h, --help\t\tprint usage\n");
+    fprintf(stderr, "\n");
+}
+
+
+/*
+ *  Prints usage instructions on option match or returns argOne
+ */
+char *validate_arg(char *argOne)
+{
+    // LOCAL VARIABLES
+    char *filename = argOne;
+    char *options[] = { "-h", "--help", NULL };
+    char **tmp_option = options;
+
+    // CHECK IT
+    while(tmp_option && *tmp_option)
+    {
+        if (strstr(argOne, *tmp_option) == argOne)
+        {
+            filename = NULL;  // It was an option, not the filename
+            print_usage();
+            break;
+        }
+        else
+        {
+            tmp_option++;
+        }
+    }
+
+    // DONE
+    return filename;
+}
+
+
+/*
+ *  Verify filename exists: 1 exists, 0 missing, -1 error
+ */
+int verify_filename(char *filename)
+{
+    // LOCAL VARIABLES
+    struct stat response;  // Used by stat()
+    int exists = 0;       // Return value
+
+    // INPUT VALIDATION
+    if (!filename || !(*filename))
+    {
+        exists = -1;
+    }
+    else
+    {
+        if (0 == stat(filename, &response))
+        {
+            exists = 1;
+        }        
+    }
+
+    // DONE
+    return exists;
+}
