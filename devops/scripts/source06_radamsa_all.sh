f4b4f0f7410db2d2d4d3bea93ab706820176f744
hark130 <hark130@yahoo.com>
2021-08-12 18:13:55 -0500

    development: Polished s06_radamsa_all.sh
    Extended STATISTICS section to include new "created files" entry.
    
    Changes to be committed:
    	modified:   devops/scripts/source06_radamsa_all.sh

--- devops/scripts/source06_radamsa_all.sh
+++ devops/scripts/source06_radamsa_all.sh
@@ -8,26 +8,23 @@
 #
 # NOTE: Consider using a tmpfs/ramdisk, as repeated writes may damage your hard disk or SSD.
 #
-NUM_INPUTS=1000
+NUM_INPUTS=2000
 clear
 
 # BASE
 devops/scripts/source06_radamsa_test_harness.sh base $NUM_INPUTS
 grep "EXECUTION: " `ls radamsa*.log | tail -n 1`
-grep -B 1 -A 3 STATISTICS `ls radamsa*.log | tail -n 1`
-# rm `ls radamsa*.log | tail -n1`
+grep -B 1 -A 4 STATISTICS `ls radamsa*.log | tail -n 1`
 echo
 
 # ASAN
 devops/scripts/source06_radamsa_test_harness.sh ASAN $NUM_INPUTS
 grep "EXECUTION: " `ls radamsa*.log | tail -n 1`
-grep -B 1 -A 3 STATISTICS `ls radamsa*.log | tail -n 1`
-# rm `ls radamsa*.log | tail -n1`
+grep -B 1 -A 4 STATISTICS `ls radamsa*.log | tail -n 1`
 echo
 
 # MEMWATCH
 devops/scripts/source06_radamsa_test_harness.sh Memwatch $NUM_INPUTS
 grep "EXECUTION: " `ls radamsa*.log | tail -n 1`
-grep -B 1 -A 3 STATISTICS `ls radamsa*.log | tail -n 1`
-# rm `ls radamsa*.log | tail -n1`
+grep -B 1 -A 4 STATISTICS `ls radamsa*.log | tail -n 1`
 echo
