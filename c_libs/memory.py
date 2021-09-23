8dffc8529d4f7bb353641811e38ef20066b1c1a4
hark130 <hark130@yahoo.com>
2021-09-23 13:01:02 -0500

    development: C-style funcs initial commit
    Basic C library functionality: strlen(), memcmp(), strstr()
    The algorithm I want to test: memstr() (Feeling cute, might rename
    later)
    
    Changes to be committed:
    	new file:   c_libs/__init__.py
    	new file:   c_libs/memory.py
    	new file:   c_libs/strings.py

---
+++ c_libs/memory.py
@@ -0,0 +1,134 @@
+from c_libs.strings import bytelen, NUL_CHAR, strstr
+from hobo.validation import validate_string, validate_type
+
+
+def memcmp(first_buff: bytes, second_buff: bytes, second_len: int) -> bool:
+    """Replicate memcmp() for bytes.
+
+    Replicates memcmp() behavior for two bytearrays.
+
+    Args:
+        first_buff: A bytearray
+        second_buff: A bytearray
+        second_len: The length of second_buff
+
+    Returns:
+        True if the two bytearrays match for the first "second_len" bytes
+
+    Raises:
+        TypeError: Bad argument type
+        ValueError: Invalid length
+        RuntimeError: second_buff is shorter than second_len
+    """
+    # LOCAL VARIABLES
+    same = True  # Set this to false if a difference is detected
+
+    # INPUT VALIDATION
+    validate_type(first_buff, 'first_buff', bytes)
+    validate_type(second_buff, 'second_buff', bytes)
+    validate_type(second_len, 'second_len', int)
+    if second_len < 0:
+        raise ValueError('second_len can not be negative')
+    if second_len > len(second_buff):
+        raise RuntimeError('Invalid usage for this test')
+
+    # DO IT
+    try:
+        for index in range(0, second_len):
+            if first_buff[index] != second_buff[index]:
+                same = False
+                break  # We can stop looking now
+    except IndexError:
+        same = False  # Apparently, first_buff wasn't long enough
+
+    # DONE
+    return same
+
+
+def memstr(haystack: bytes, needle: bytes, needle_len: int) -> bool:
+    """Match needle in haystack.
+
+    Attempts to find an occurrence of needle, up to needle_len in size, inside haystack.
+    If needle contains a nul-terminator, this function will investigate memory past
+    haystack's nul-terminator.
+
+    Args:
+        haystack: A nul-terminated bytearray, of indeterminate size, to search for needle
+        needle: A raw buffer to search haystack for
+        needle_len: The length of needle
+
+    Returns:
+        True if needle is found in haystack
+
+    Raises:
+        TypeError: Bad argument type
+        ValueError: Bad values
+    """
+    # LOCAL VARIABLES
+    nul_char_found = False  # Life is different if needle contains a nul character
+    match = False           # Return value
+
+    # INPUT VALIDATION
+    validate_type(haystack, 'haystack', bytes)
+    validate_type(needle, 'needle', bytes)
+    validate_type(needle_len, 'needle_len', int)
+    if needle_len < 0:
+        raise ValueError('needle_len can not be negative')
+
+    # DO IT
+    # 1. Is there a nul character in needle within needle_len range?
+    try:
+        if needle_len > bytelen(needle):
+            nul_char_found = True
+    except ValueError:
+        nul_char_found = False
+
+    # 2. Search for a match
+    if nul_char_found:
+        match = _nul_search(haystack, needle, needle_len)
+    else:
+        match = _non_nul_search(haystack, needle, needle_len)
+
+    # DONE
+    return match
+
+
+def _non_nul_search(haystack: bytes, needle: bytes, needle_len: int) -> bool:
+    """Match needle in haystack.
+
+    Attempts to find an occurrence of needle, up to needle_len in size, inside haystack.
+    This function does not validate type or value above what is covered under Raises.
+
+    Args:
+        haystack: A nul-terminated bytearray, of indeterminate size, to search for needle
+        needle: A raw buffer, non-nul terminated, to search haystack for
+        needle_len: The length of needle
+
+    Returns:
+        True if needle is found in haystack, False otherwise
+
+    Raises:
+        RuntimeError: needle contains a nul character within needle_len
+    """
+    # LOCAL VARIABLES
+    found_it = False                 # Needle was found in haystack
+    haystack_len = bytelen(haystack)  # Length of haystack
+
+    # INPUT VALIDATION
+    if strstr(needle[:needle_len], NUL_CHAR):
+        raise RuntimeError('needle contains a nul character.  '
+                           'Call _nul_search() instead.')
+
+    # DO IT
+    if haystack_len >= needle_len:
+        for offset in range(0, haystack_len - needle_len + 1):
+            found_it = memcmp(haystack[offset:], needle, needle_len)
+            if found_it:
+                break
+
+    # DONE
+    return found_it
+
+
+def _nul_search(haystack: bytes, needle: bytes, needle_len: int) -> bool:
+    return False
---
+++ c_libs/strings.py
@@ -0,0 +1,74 @@
+from hobo.validation import validate_string, validate_type
+
+
+
+NUL_CHAR = bytes(chr(0), 'ascii')  # Nul character
+
+
+def bytelen(string: bytes) -> int:
+    """Replicate strlen() for bytes.
+
+    Replicates strlen() behavior for a bytearray.
+
+    Args:
+        string: A nul-terminated bytearray
+
+    Returns:
+        The number of non-nul bytes preceding the nul character
+
+    Raises:
+        TypeError: Bad argument type
+        ValueError: No nul-character found in string
+    """
+    # LOCAL VARIABLES
+    length = 0                         # Return value
+
+    # INPUT VALIDATION
+    validate_type(string, 'string', bytes)
+    if NUL_CHAR not in string:
+        raise ValueError('This is not a nul-terminated bytearray')
+
+    # MEASURE IT
+    for char in string:
+        # print(f'CHAR: {char}')  # DEBUGGING
+        if 0 == char:
+            # print('FOUND A NUL CHAR!')  # DEBUGGING
+            break  # Found it
+        else:
+            length += 1
+    if length == len(string):
+        # print(f'\nLENGTH: {length}\nlen(string): {len(string)}')  # DEBUGGING
+        # print(f'bytelen({string}')  # DEBUGGING
+        raise RuntimeError('string passed validation but no nul character was found?!')
+
+    # DONE
+    return length
+
+
+def strstr(haystack: bytes, needle: bytes) -> bool:
+    """Replicate strstr() for bytes.
+
+    Replicates strstr() behavior for bytearrays in a Pythonic way.  We're not testing
+    strstr() here so there's no concern over nul-termination.  If stsrstr() were
+    called with one or more non-nul teriminated buffers, they would likely not match
+    anyway.
+
+    Args:
+        haystack: A bytearray in which to search for the needle
+        needle: A bytearray to find in the haystack
+
+    Returns:
+        True if needle is found in haystack, False otherwise
+
+    Raises:
+        TypeError: Bad argument type
+        ValueError: needle is empty
+    """
+    # INPUT VALIDATION
+    validate_type(haystack, 'haystack', bytes)
+    validate_type(needle, 'needle', bytes)
+    if not needle:
+        raise ValueError('needle can not be empty')
+
+    # DO IT
+    return needle in haystack
