91022fb9df1f9a3a3badd819d9bf51b121ab6d91
hark130 <hark130@yahoo.com>
2021-09-23 15:35:45 -0500

    development: Finished(?) writing 46 unit tests
    42 passing
    4 skipped (MemoryError)
    
    I ran out of valid ideas so I stopped writing unit tests
    The memstr() algorithm appears to be working perfectly
    
    Changes to be committed:
    	modified:   test/unit_tests/test_memstr.py

--- test/unit_tests/test_memstr.py
+++ test/unit_tests/test_memstr.py
@@ -21,6 +21,7 @@ from test.unit_tests.helper_functions import append_noise, byteify_a_string, ran
 from test.unit_tests.unittestbase import execute_test_cases
 from test.unit_tests.unittestcallable import UnitTestCallable
 from c_libs.memory import memstr
+from c_libs.strings import NUL_CHAR
 
 
 class TestMemstr(UnitTestCallable):
@@ -50,7 +51,7 @@ class TestMemstr(UnitTestCallable):
         super().__init__(*args, **kwargs)
 
         # ATTRIBUTES
-        # TD: DDN... Put them here
+        self.max_int_size = sys.maxsize  # https://docs.python.org/3.1/library/sys.html#sys.maxsize
 
     def setUp(self) -> None:
         """Prepares Test Case.
@@ -69,7 +70,9 @@ class TestMemstr(UnitTestCallable):
         super(TestMemstr, self).setUp()
 
         # INPUT VALIDATION
-        # TD: DDN... validate attributes
+        self._validate_type(self.max_int_size, 'self.max_int_size', int)
+        if 0 >= self.max_int_size:
+            raise ValueError(f'Invalid max_int_size of {self.max_int_size}')
 
     def call_callable(self) -> Any:
         """Calls memstr.
@@ -139,6 +142,14 @@ class NormalTestMemstr(TestMemstr):
         self.expect_return(False)
         self.run_test()
 
+    def test_normal_04(self):
+        """Basic input, no match."""
+        haystack = byteify_a_string('Your time is up, my time is now')
+        needle = b"You can't see me"
+        self.set_test_input(haystack=haystack, needle=needle, needle_len=len(needle))
+        self.expect_return(False)
+        self.run_test()
+
 
 class ErrorTestMemstr(TestMemstr):
     """Error Test Cases.
@@ -172,6 +183,22 @@ class ErrorTestMemstr(TestMemstr):
         self.expect_exception(ValueError, '')
         self.run_test()
 
+    def test_error_05(self):
+        """needle is empty"""
+        needle = b''
+        haystack = byteify_a_string('What will happen?!')
+        self.set_test_input(haystack=haystack, needle=needle, needle_len=len(needle))
+        self.expect_exception(RuntimeError, 'A C buffer would never be empty')
+        self.run_test()
+
+    def test_error_06(self):
+        """haystack is *all the way* empty"""
+        needle = b'Lost and found'
+        haystack = b''
+        self.set_test_input(haystack=haystack, needle=needle, needle_len=len(needle))
+        self.expect_exception(ValueError, 'This is not a nul-terminated bytearray')
+        self.run_test()
+
 
 class BoundaryTestMemstr(TestMemstr):
     """Boundary Test Cases.
@@ -179,19 +206,108 @@ class BoundaryTestMemstr(TestMemstr):
     Organize the Boundary Test Cases
     """
 
-    @unittest.skip('TD: DDN... Implement later')
     def test_boundary_01(self):
-        """Boundary input: max int"""
-        # self.set_test_input(MAX_INT_VALUE - 90, 90)
-        # self.expect_return(MAX_INT_VALUE)
-        # self.run_test()
+        """One-off test: non-nul needle is barely not a match (beginning)"""
+        haystack = byteify_a_string('You better not find this')
+        needle = b'you'
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
 
-    @unittest.skip('TD: DDN... Implement later')
     def test_boundary_02(self):
-        """Boundary input: max int (negative)"""
-        # self.set_test_input(-MAX_INT_VALUE + 90, -90)
-        # self.expect_return(-MAX_INT_VALUE)
-        # self.run_test()
+        """One-off test: non-nul needle is barely not a match (end)"""
+        haystack = byteify_a_string('You better not find this')
+        needle = b'hiS'
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_boundary_03(self):
+        """haystack match is at the end"""
+        haystack = byteify_a_string('You better find this')
+        needle = b'You'
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_boundary_04(self):
+        """haystack match is at the beginning"""
+        haystack = byteify_a_string('You better find this')
+        needle = b'this'
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_boundary_05(self):
+        """haystack and needle are an exact match"""
+        test_input = b'You better find this'
+        self.set_test_input(haystack=test_input + NUL_CHAR, needle=test_input,
+                            needle_len=len(test_input))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_boundary_06(self):
+        """haystack and needle are an exact match precending haystack's nul char"""
+        test_input = 'You better find this'
+        needle = bytes(test_input, 'ascii')
+        haystack = byteify_a_string(test_input)
+        self.set_test_input(haystack=haystack, needle=needle, needle_len=len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_boundary_08(self):
+        """haystack is empty"""
+        needle = b'Lost and found'
+        haystack = NUL_CHAR + randomize_bytes()
+        self.set_test_input(haystack=haystack, needle=needle, needle_len=len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_boundary_09(self):
+        """haystack is just a nul character without noise"""
+        needle = b'Lost and found'
+        haystack = NUL_CHAR
+        self.set_test_input(haystack=haystack, needle=needle, needle_len=len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    @unittest.skip('This test case raises a MemoryError exception')
+    def test_boundary_10(self):
+        """Needle exists; needle length is max int - 1"""
+        needle = b'?' * (self.max_int_size - 1)
+        haystack = byteify_a_string('This is my string.  I found it here: ' + needle +
+                                    'Can you see it?')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    @unittest.skip('This test case raises a MemoryError exception')
+    def test_boundary_11(self):
+        """Needle missing; needle length is max int - 1"""
+        needle = b'?' * (self.max_int_size - 1)
+        haystack = byteify_a_string('!' * (self.max_int_size + 10))
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    @unittest.skip('This test case raises a MemoryError exception')
+    def test_boundary_12(self):
+        """Needle exists; needle length is max int - 1"""
+        needle = b'?' * self.max_int_size
+        haystack = byteify_a_string('This is my string.  I found it here: ' + needle +
+                                    'Can you see it?')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    @unittest.skip('This test case raises a MemoryError exception')
+    def test_boundary_13(self):
+        """Needle missing; needle length is max int - 1"""
+        needle = b'?' * self.max_int_size
+        haystack = byteify_a_string('!' * (self.max_int_size + 10))
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
 
 
 class SpecialTestMemstr(TestMemstr):
@@ -200,12 +316,224 @@ class SpecialTestMemstr(TestMemstr):
     Organize the Special Test Cases
     """
 
-    @unittest.skip('TD: DDN... Implement later')
     def test_special_01(self):
-        """Boundary input: max int"""
-        # self.set_test_input(0, 0)
-        # self.expect_return(0)
-        # self.run_test()
+        """Existing needle contains a nul: leading"""
+        needle = NUL_CHAR + b'My string'
+        haystack = append_noise(
+            b'Have you seen my string?' + needle + b' is around here somewhere')
+        # print(f'NEEDLE: {needle}\nHAYSTACK: {haystack}')  # DEBUGGING
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_02(self):
+        """Missing needle contains a nul: leading"""
+        needle = NUL_CHAR + b'My missing string'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_03(self):
+        """Existing needle contains a nul: trailing"""
+        needle = b'My string' + NUL_CHAR
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_04(self):
+        """Missing needle contains a nul: trailing"""
+        needle = b'My missing string' + NUL_CHAR
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_05(self):
+        """Existing needle contains a nul: middle"""
+        needle = b'My string' + NUL_CHAR + b' is still missing'
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' and is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_06(self):
+        """Missing needle contains a nul: middle"""
+        needle = b'My missing string' + NUL_CHAR + b' still has not been found'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.'
+                                    '  Apparently, it has gone missing!  Let me know if you'
+                                    ' see it.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_07(self):
+        """Existing needle contains a nul: leading and middle"""
+        needle = b'My string' + NUL_CHAR + b' is still missing'
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' and is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_08(self):
+        """Missing needle contains a nul: leading and middle"""
+        needle = b'My missing string' + NUL_CHAR + b' still has not been found'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.'
+                                    '  Apparently, it has gone missing!  Let me know if you'
+                                    ' see it.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_09(self):
+        """Existing needle contains a nul: middle and trailing"""
+        needle = b'My string' + NUL_CHAR + b' is still missing'
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' and is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_10(self):
+        """Missing needle contains a nul: middle and trailing"""
+        needle = b'My missing string' + NUL_CHAR + b' still has not been found'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.'
+                                    '  Apparently, it has gone missing!  Let me know if you'
+                                    ' see it.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_11(self):
+        """Existing needle contains a nul: leading and trailing"""
+        needle = b'My string' + NUL_CHAR + b' is still missing'
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' and is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_12(self):
+        """Missing needle contains a nul: leading and trailing"""
+        needle = b'My missing string' + NUL_CHAR + b' still has not been found'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.'
+                                    '  Apparently, it has gone missing!  Let me know if you'
+                                    ' see it.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_13(self):
+        """Existing needle contains a nul: leading, middle, and trailing"""
+        needle = b'My string' + NUL_CHAR + b' is still missing'
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' and is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_14(self):
+        """Missing needle contains a nul: leading, middle, and trailing"""
+        needle = b'My missing string' + NUL_CHAR + b' still has not been found'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.'
+                                    '  Apparently, it has gone missing!  Let me know if you'
+                                    ' see it.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_15(self):
+        """Existing needle contains a nul: numerous"""
+        needle = b'My string' + NUL_CHAR + b' is still missing'
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b' and is around here somewhere')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_16(self):
+        """Missing needle contains a nul: numerous"""
+        needle = b'My missing string' + NUL_CHAR + b' still has not been found'
+        haystack = byteify_a_string('Have you seen my string?  I thought I left it here.'
+                                    '  Apparently, it has gone missing!  Let me know if you'
+                                    ' see it.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_17(self):
+        """Existing needle contains a nul: all"""
+        needle = NUL_CHAR * 10
+        haystack = append_noise(
+            b'Have you seen my string?  ' + needle + b'  I think I left it here.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_18(self):
+        """Missing needle contains a nul: all"""
+        needle = NUL_CHAR * 10
+        haystack = append_noise(
+            b'Have you seen my string?  ' + NUL_CHAR * 9 + b'  I think I left it here.')
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
+
+    def test_special_19(self):
+        """Non-prepended haystack, existing needle, no nul character in needle"""
+        needle = b'This is the needle'
+        haystack = append_noise(needle)
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_20(self):
+        """Non-prepended haystack, missing needle, nul character in needle"""
+        needle = b'Can you find ' + NUL_CHAR + b' THE RING?!'
+        haystack = append_noise(needle)
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_21(self):
+        """Needle is nul-terminated but nul character outside needle_len range"""
+        test_input = b'Ignore this nul character'
+        needle = test_input + NUL_CHAR
+        haystack = append_noise(b'What did I tell you?!  ' + needle + b' or else!')
+        self.set_test_input(haystack, needle, len(test_input))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_22(self):
+        """Needle is nul-terminated but nul character outside needle_len range and missing"""
+        test_input = b'Ignore this nul character'
+        needle = test_input + NUL_CHAR
+        haystack = append_noise(b'What did I tell you?!  ' + test_input + b' or else!')
+        self.set_test_input(haystack, needle, len(test_input))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_23(self):
+        """Needle contains non-matching noise outside needle_len range"""
+        test_input = b'Ignore this noise'
+        needle = test_input + randomize_bytes()
+        haystack = append_noise(b'What did I tell you?!  ' + test_input + b' or else!')
+        self.set_test_input(haystack, needle, len(test_input))
+        self.expect_return(True)
+        self.run_test()
+
+    def test_special_24(self):
+        """Ensure non-nul needle doesn't match after the haystack nul character"""
+        haystack = byteify_a_string('This is my string.')
+        haystack += NUL_CHAR + b'There are many like it but this one is mine.'
+        needle = b'this one is mine'
+        self.set_test_input(haystack, needle, len(needle))
+        self.expect_return(False)
+        self.run_test()
 
 
 if __name__ == '__main__':
