4a4f5ee44af471f79164c0b81298db77b8f1fc08
hark130 <hark130@yahoo.com>
2021-11-15 14:27:55 -0600

    development: Updated test harness test script
    Test script now prints out test cases
    It also watches the watch directory
    "Who watches the watch directory?"
    
    SPOILER ALERT: The current code base, soon to be committed, successfully
    passed 217K test cases where: a file was created, the daemon "did
    something"(?), the file was matched by the test harness and deleted.
    
    Changes to be committed:
    	modified:   devops/scripts/filename_test.sh
    
    Changes not staged for commit:
    	modified:   src/HARE_library.c
    	modified:   src/HARE_library.h
    	modified:   src/source08_test_harness.c

--- devops/scripts/filename_test.sh
+++ devops/scripts/filename_test.sh
@@ -1,10 +1,16 @@
 make filename_test
+make source08
 
+rm -f /tmp/watch/*
 rm -f /tmp/watch/processed/*
-while [[ `ls /tmp/watch/processed | wc -c` -eq 0 ]]
+TEST_NUM=1
+while [[ `ls /tmp/watch/processed | wc -l` -eq 0 ]] && [[ `ls /tmp/watch | wc -l` -eq 1 ]]
 do
-	echo -n "some_filename.txt" | radamsa > filename_test.txt && sudo ./dist/filename_test_best.bin filename_test.txt
+	echo "Test: $TEST_NUM"
+	echo -n "some_filename.txt" | radamsa > filename_test.txt && sudo $1 filename_test.txt
+	((TEST_NUM=TEST_NUM+1))
 done
+ls /tmp/watch/
 ls /tmp/watch/processed/
 
 #echo -n "some_filename.txt" | radamsa > filename_test.txt && sudo ./dist/filename_test_best.bin filename_test.txt
