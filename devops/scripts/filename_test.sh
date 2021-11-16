b82943b311317859d0c7d42ef5ebd874110b73c4
hark130 <hark130@yahoo.com>
2021-11-16 13:12:17 -0600

    development: Fixed filename_test.sh BUGs
    - Fixed "environment setup" oversight
    - Fixed ASAN log path oversight
    - Cleaned up the user output to be more readable
    
    Changes to be committed:
    	modified:   devops/scripts/filename_test.sh

--- devops/scripts/filename_test.sh
+++ devops/scripts/filename_test.sh
@@ -2,6 +2,7 @@ make filename_test
 make source08
 
 LOG_DIR=/tmp/log/
+LOG_FILENAME=asan_log
 WATCH_DIR=/tmp/watch/
 PROCESS_DIR=/tmp/watch/processed/
 TEST_NUM=1
@@ -10,7 +11,7 @@ TEST_NUM=1
 # Log
 if [ -d $LOG_DIR ]
 then
-    rm -f $LOG_DIR
+    rm -f $LOG_DIR*
 else
 	mkdir $WATCH_DIR
 fi
@@ -33,14 +34,17 @@ fi
 while [ `ls $LOG_DIR | wc -l` -eq 0 ] && [ `ls $WATCH_DIR | wc -l` -eq 1 ] && [ `ls $PROCESS_DIR | wc -l` -eq 0 ]
 do
 	echo "Test: $TEST_NUM"
-	echo -n "some_filename.txt" | radamsa > filename_test.txt && sudo ASAN_OPTIONS="log_path=$LOG_DIR" $1 filename_test.txt
+	echo -n "some_filename.txt" | radamsa > filename_test.txt && sudo ASAN_OPTIONS="log_path=$LOG_DIR$LOG_FILENAME" $1 filename_test.txt
 	TEST_NUM=$((TEST_NUM+1))
 done
 echo "$LOG_DIR"
 ls $LOG_DIR
+echo
 echo "$WATCH_DIR"
 ls $WATCH_DIR
+echo
 echo "$PROCESS_DIR"
 ls $PROCESS_DIR
+echo
 
 #echo -n "some_filename.txt" | radamsa > filename_test.txt && sudo ./dist/filename_test_best.bin filename_test.txt
